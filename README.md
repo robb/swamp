4b50599d359aa4e038ac59744a0b0afaa51d38e0
Robert Böhnke <robb@robb.is>
2014-06-15 20:11:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# swamp
+
+Icon stamping in Swift with zero dependencies. A lovely addition to every build
+process.
+
+## How to use
+
+
+1. Switch your Xcode using `sudo xcode-select -switch /Applications/Xcode6-Beta.app/`
+2. Call swamp like so: `./swamp.swift -- input.png output.png "Hello World"`  
+   If you're seeing crazy unicode noise or `<unknown>:0: no such file or directory: 'input.png'`, you most likely forgot the `--`.
+
+<img src="./example.png">
+
+Ported from [stamp](https://github.com/jberkel/stamp) by [Jan
+Berkel](https://github.com/jberkel).
+
---
+++ swamp.swift
@@ -0,0 +1,115 @@
+#!/usr/bin/xcrun --sdk macosx -r swift -i
+
+import AppKit
+
+extension NSImage {
+    func lockFocus(flipped: Bool, block: () -> ()) {
+        self.lockFocusFlipped(true)
+        block()
+        self.unlockFocus()
+    }
+
+    func bitmapRepresentation() -> NSBitmapImageRep {
+        let cgRef = self.CGImageForProposedRect(nil, context: nil, hints: nil)
+        let result = NSBitmapImageRep(CGImage: cgRef.takeUnretainedValue())
+
+        cgRef.release()
+
+        return result
+    }
+}
+
+struct Swamp {
+    let icon: NSImage
+    let textAttributes: Dictionary<String, AnyObject>
+    let fontName = "HelveticaNeue-Light"
+
+    init(icon: NSImage) {
+        self.icon = icon
+        self.textAttributes = [
+            NSShadowAttributeName: {
+                let unit = icon.size.height / 64
+
+                let shadow = NSShadow()
+                shadow.shadowOffset = CGSizeMake(0, -unit)
+                shadow.shadowBlurRadius = unit
+                shadow.shadowColor = NSColor.controlDarkShadowColor()
+                return shadow
+            }(),
+            NSParagraphStyleAttributeName: {
+                let paragraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as NSMutableParagraphStyle!
+                paragraphStyle.alignment = NSTextAlignment.CenterTextAlignment
+                paragraphStyle.lineBreakMode = NSLineBreakMode.ByWordWrapping;
+                return paragraphStyle
+            }(),
+            NSForegroundColorAttributeName: NSColor.whiteColor()
+        ]
+    }
+
+    func addText(text: String) {
+        let offset = self.icon.size.height / 20
+        let containerSize = CGSizeMake(self.icon.size.width,
+                                       self.icon.size.height - 2 * offset)
+        var fontSize = self.icon.size.height / 4
+
+        var textContainer: NSTextContainer
+        var textStorage: NSTextStorage
+        var layoutManager: NSLayoutManager
+
+        var renderedRange: NSRange
+        var usedRect: CGRect
+
+        // Keep decreasing the font size until it's either 8 pts or the text
+        // fits completely
+        do {
+            textContainer = NSTextContainer(containerSize: containerSize)
+
+            textStorage = NSTextStorage(string: text, attributes:textAttributes)
+            textStorage.font = NSFont(name:self.fontName, size: fontSize)
+
+            layoutManager = NSLayoutManager()
+            layoutManager.addTextContainer(textContainer)
+            layoutManager.textStorage = textStorage
+
+            renderedRange = layoutManager.glyphRangeForTextContainer(textContainer)
+            usedRect = layoutManager.usedRectForTextContainer(textContainer)
+
+            fontSize -= 0.1
+        } while renderedRange.length < countElements(text) && fontSize > 8
+
+        let point = CGPointMake(0, self.icon.size.height - usedRect.size.height - offset)
+
+        self.icon.lockFocus(false) {
+            layoutManager.drawGlyphsForGlyphRange(renderedRange, atPoint:point)
+        }
+    }
+
+    func save(path: String) {
+        let data = self.icon
+            .bitmapRepresentation()
+            .representationUsingType(NSBitmapImageFileType.NSPNGFileType,
+                                     properties: nil)
+
+        data.writeToFile(path, atomically:true)
+    }
+}
+
+if countElements(Process.arguments) < 3 {
+    println("Usage: stamp.swift -- [input] [output] [text]")
+    exit(1)
+}
+
+let input = Process.arguments[0]
+let output = Process.arguments[1]
+let text = Process.arguments[2]
+
+let icon: NSImage? = NSImage(data: NSData(contentsOfFile: input))
+
+if icon == nil {
+    println("Could not load file \(input)")
+    exit(1)
+}
+
+let swamp = Swamp(icon: icon!)
+swamp.addText(text)
+swamp.save(output)
